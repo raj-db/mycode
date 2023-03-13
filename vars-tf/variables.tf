635b1f03a7a6c28952a3debaee71a139ed01b67a
raj-db <rajesh.b1419@gmail.com>
2023-03-13 19:51:37 +0000

    intro to tfvars

---
+++ vars-tf/variables.tf
@@ -0,0 +1,21 @@
+variable "container_name" {
+  description = "Value of the name for the Docker container"
+  # basic types include string, number and bool
+  type        = string
+  default     = "ExampleNginxContainer"
+}
+
+variable "internal_port" {
+  description = "Value for internal port"
+  # basic types include string, number and bool
+  type        = number
+  default     = 80
+}
+
+variable "external_port" {
+  description = "Value for external port"
+  # basic types include string, number and bool
+  type        = number
+  default     = 2224
+}
+
